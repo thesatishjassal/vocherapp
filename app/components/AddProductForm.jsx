dcc55e44a3a0c1efe0facabadaa17da8f6758fcd
Satish jassal <thesatishjassal@gmail.com>
2025-02-19 17:48:28 +0530

    code

--- app/components/AddProductForm.jsx
+++ app/components/AddProductForm.jsx
@@ -136,7 +136,7 @@ const AddProductForm = () => {
                       <option value="">Select Category</option>
                       {categories.map((cat) => (
                         <option key={cat.id} value={cat.id}>
-                          {cat.name}
+                          {cat.catname}
                         </option>
                       ))}
                     </select>
@@ -151,7 +151,7 @@ const AddProductForm = () => {
                       <option value="">Select Subcategory</option>
                       {subCategories.map((sub) => (
                         <option key={sub.id} value={sub.id}>
-                          {sub.name}
+                          {sub.subcatname}
                         </option>
                       ))}
                     </select>
