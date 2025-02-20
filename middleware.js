353c11c3c647c73049c6faa58eead3f64094d713
Satish jassal <thesatishjassal@gmail.com>
2025-02-20 15:36:31 +0530

    code

---
+++ middleware.js
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req) {
+  const protectedRoutes = [
+    "/products",
+    "/addclient",
+    "/addinvoice",
+    "/addoutinvoice",
+    "/quotation",
+    "/report",
+  ];
+
+  const userDetails = req.cookies.get("user_details")?.value;
+
+  // Check if the route is protected
+  if (protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route))) {
+    if (!userDetails) {
+      return NextResponse.redirect(new URL("/login", req.url)); // Redirect to login
+    }
+  }
+
+  return NextResponse.next();
+}
+
+// Apply middleware only to the specified routes
+export const config = {
+  matcher: ["/products", "/addclient", "/addinvoice", "/addoutinvoice", "/quotation", "/report"],
+};
