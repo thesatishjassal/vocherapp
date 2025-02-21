aed8a2bcdd740fec60971596e83850436da19e7b
Satish jassal <thesatishjassal@gmail.com>
2025-02-21 16:11:56 +0530

    code

--- app/components/ImageUploadModal.jsx
+++ app/components/ImageUploadModal.jsx
@@ -28,14 +28,14 @@ const ImageUploadModal = ({ show, onClose, product, onUpload }) => {
         toast.error("❌ Only image files are allowed!");
         return;
       }
-      // if (selectedFile.size < MIN_FILE_SIZE) {
-      //   toast.error("⚠️ File size is too small. Please select an image of at least 500KB.");
-      //   return;
-      // }
-      // if (selectedFile.size > MAX_FILE_SIZE) {
-      //   toast.error("⚠️ File size exceeds 2MB. Please select a smaller image.");
-      //   return;
-      // }
+      if (selectedFile.size < MIN_FILE_SIZE) {
+        toast.error("⚠️ File size is too small. Please select an image of at least 500KB.");
+        return;
+      }
+      if (selectedFile.size > MAX_FILE_SIZE) {
+        toast.error("⚠️ File size exceeds 2MB. Please select a smaller image.");
+        return;
+      }
 
       setPreview(URL.createObjectURL(selectedFile));
       setFile(selectedFile);
