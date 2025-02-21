c6ebcd671e1a80aff78ffc9fc7f282540db306db
Satish jassal <thesatishjassal@gmail.com>
2025-02-21 17:45:16 +0530

    code

--- app/components/ImageUploadModal.jsx
+++ app/components/ImageUploadModal.jsx
@@ -9,7 +9,8 @@ const MAX_FILE_SIZE = 2 * 1024 * 1024; // 1 MB
 
 const ImageUploadModal = ({ show, onClose, product, onUpload }) => {
   const [file, setFile] = useState(null);
-  const [preview, setPreview] = useState(product?.thumbnail || "");
+  const ImageUrl = "https://api.panvic.in/" + product?.thumbnail;
+  const [preview, setPreview] = useState(ImageUrl);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
