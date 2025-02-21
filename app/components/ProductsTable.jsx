fd61e66bb64b80595dea08c6257084aaae0d6f1e
Satish jassal <thesatishjassal@gmail.com>
2025-02-21 16:48:25 +0530

    code

--- app/components/ProductsTable.jsx
+++ app/components/ProductsTable.jsx
@@ -142,7 +142,7 @@ const ProductsTable = () => {
               </tr>
             </thead>
             <tbody>
-              {products.length == 0 ? <img src="/assets/img/no-product-found.png" /> : products.map((product, index) => (
+              {products.length == 0 ? <img src="/assets/img/no-product-found.png" className="no-product-found" /> : products.map((product, index) => (
                 <tr key={index}>
                   <td>{product.id}</td>
                   <td>
@@ -206,7 +206,6 @@ const ProductsTable = () => {
                   </td>
                 </tr>
               ))}
-            
             </tbody>
           </table>
         </div>
