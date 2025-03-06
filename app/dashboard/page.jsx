c7c727e79a273a9b1a7d532f13e940cfc3d39ac3
Satish jassal <thesatishjassal@gmail.com>
2025-03-06 16:10:38 +0530

    code

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -8,158 +8,115 @@ export default function Home() {
         <p className="text-sm mb-0">Let’s make today amazing! 🚀</p>
       </div>
       <div className="row mb-4">
-        {/* Add Clients */}
-        <div className="col-lg-2 col-md-3 col-12">
+        <div className="col-lg-2 col-md-4 col-6 mb-3">
           <a href="/addclient">
             <div className="card">
               <span className="mask opacity-10 border-radius-lg"></span>
-              <div className="card-body p-3 position-relative">
-                <div className="row">
-                  <div className="col-12 text-center">
-                    <div className="icon_wrapper">
-                      <img
-                        src="https://freedesignfile.com/upload/2023/09/Businessman-3D-professions-icon-vector.jpg"
-                        alt=""
-                        className="client_img"
-                      />
-                    </div>
-                    <h5 className="font-weight-bolder mb-0 mt-3">
-                      Add Clients
-                    </h5>
-                    <span className="count text-sm">1600</span>
-                  </div>
+              <div className="card-body p-3 position-relative text-center">
+                <div className="icon_wrapper">
+                  <img
+                    src="https://freedesignfile.com/upload/2023/09/Businessman-3D-professions-icon-vector.jpg"
+                    alt=""
+                    className="client_img"
+                  />
                 </div>
+                <h5 className="font-weight-bolder mb-0 mt-3">Add Clients</h5>
+                <span className="count text-sm">1600</span>
               </div>
-              <p className="bottom_lablel">Master Head</p>
             </div>
           </a>
         </div>
 
-        {/* Add Invoice */}
-        <div className="col-lg-2 col-md-2 col-12 mt-4 mt-md-0">
+        <div className="col-lg-2 col-md-4 col-6 mb-3">
           <a href="/products">
             <div className="card">
               <span className="mask opacity-10 border-radius-lg"></span>
-              <div className="card-body p-3 position-relative">
-                <div className="row">
-                  <div className="col-12 text-center">
-                    <div className="icon_wrapper">
-                      <img
-                        src="https://cdn3d.iconscout.com/3d/premium/thumb/product-3d-icon-download-in-png-blend-fbx-gltf-file-formats--tag-packages-box-marketing-advertisement-pack-branding-icons-4863042.png?f=webp"
-                        alt=""
-                        className="client_img"
-                      />
-                    </div>
-                    <h5 className="font-weight-bolder mb-0 mt-3">Add Stocks</h5>
-                    <span className="count text-sm">100</span>
-                  </div>
+              <div className="card-body p-3 position-relative text-center">
+                <div className="icon_wrapper">
+                  <img
+                    src="https://cdn3d.iconscout.com/3d/premium/thumb/product-3d-icon-download-in-png-blend-fbx-gltf-file-formats--tag-packages-box-marketing-advertisement-pack-branding-icons-4863042.png?f=webp"
+                    alt=""
+                    className="client_img"
+                  />
                 </div>
+                <h5 className="font-weight-bolder mb-0 mt-3">Add Stocks</h5>
+                <span className="count text-sm">100</span>
               </div>
-              <p className="bottom_lablel">Master Head</p>
             </div>
           </a>
         </div>
 
-        {/* Add Outinvoice */}
-        <div className="col-lg-2 col-md-2 col-12">
+        <div className="col-lg-2 col-md-4 col-6 mb-3">
           <a href="/getinvouchers">
-            {" "}
             <div className="card">
               <span className="mask opacity-10 border-radius-lg"></span>
-              <div className="card-body p-3 position-relative">
-                <div className="row">
-                  <div className="col-12 text-center">
-                    <div className="icon_wrapper">
-                      <img
-                        src="https://cdn3d.iconscout.com/3d/premium/thumb/receipt-3d-illustration-download-in-png-blend-fbx-gltf-file-formats--product-invoice-purchase-record-bill-business-pack-finance-illustrations-4280960.png?f=webp"
-                        alt=""
-                        className="client_img"
-                      />
-                    </div>
-                    <h5 className="font-weight-bolder mb-0 mt-3">
-                      Add In Vocher
-                    </h5>
-                    <span className="count text-sm">100</span>
-                  </div>
+              <div className="card-body p-3 position-relative text-center">
+                <div className="icon_wrapper">
+                  <img
+                    src="https://cdn3d.iconscout.com/3d/premium/thumb/receipt-3d-illustration-download-in-png-blend-fbx-gltf-file-formats--product-invoice-purchase-record-bill-business-pack-finance-illustrations-4280960.png?f=webp"
+                    alt=""
+                    className="client_img"
+                  />
                 </div>
+                <h5 className="font-weight-bolder mb-0 mt-3">Add In Voucher</h5>
+                <span className="count text-sm">100</span>
               </div>
             </div>
           </a>
         </div>
 
-        {/* Add Warehouse */}
-        <div className="col-lg-2 col-md-2 col-12 mt-4 mt-md-0">
+        <div className="col-lg-2 col-md-4 col-6 mb-3">
           <a href="/getoutvouchers">
-            {" "}
             <div className="card">
               <span className="mask opacity-10 border-radius-lg"></span>
-              <div className="card-body p-3 position-relative">
-                <div className="row">
-                  <div className="col-12 text-center">
-                    <div className="icon_wrapper">
-                      <img
-                        src="https://cdn3d.iconscout.com/3d/premium/thumb/order-list-3d-icon-download-in-png-blend-fbx-gltf-file-formats--logistic-checklist-currier-product-pack-e-commerce-shopping-icons-6159358.png"
-                        alt=""
-                        className="client_img"
-                      />
-                    </div>
-                    <h5 className="font-weight-bolder mb-0 mt-3">
-                      Out Voucher
-                    </h5>
-                    <span className="count text-sm">100</span>
-                  </div>
+              <div className="card-body p-3 position-relative text-center">
+                <div className="icon_wrapper">
+                  <img
+                    src="https://cdn3d.iconscout.com/3d/premium/thumb/order-list-3d-icon-download-in-png-blend-fbx-gltf-file-formats--logistic-checklist-currier-product-pack-e-commerce-shopping-icons-6159358.png"
+                    alt=""
+                    className="client_img"
+                  />
                 </div>
+                <h5 className="font-weight-bolder mb-0 mt-3">Out Voucher</h5>
+                <span className="count text-sm">100</span>
               </div>
             </div>
           </a>
         </div>
-        {/* Add Warehouse */}
-        <div className="col-lg-2 col-md-2 col-12 mt-4 mt-md-0">
+
+        <div className="col-lg-2 col-md-4 col-6 mb-3">
           <a href="/report">
-            {" "}
             <div className="card">
               <span className="mask opacity-10 border-radius-lg"></span>
-              <div className="card-body p-3 position-relative">
-                <div className="row">
-                  <div className="col-12 text-center">
-                    <div className="icon_wrapper">
-                      <img
-                        src="https://cdn3d.iconscout.com/3d/premium/thumb/business-report-3d-icon-download-in-png-blend-fbx-gltf-file-formats--document-clipboard-data-pack-icons-9291057.png?f=webp"
-                        alt=""
-                        className="client_img"
-                      />
-                    </div>
-                    <h5 className="font-weight-bolder mb-0 mt-3">
-                      Make Reports
-                    </h5>
-                    <span className="count text-sm">100</span>
-                  </div>
+              <div className="card-body p-3 position-relative text-center">
+                <div className="icon_wrapper">
+                  <img
+                    src="https://cdn3d.iconscout.com/3d/premium/thumb/business-report-3d-icon-download-in-png-blend-fbx-gltf-file-formats--document-clipboard-data-pack-icons-9291057.png?f=webp"
+                    alt=""
+                    className="client_img"
+                  />
                 </div>
+                <h5 className="font-weight-bolder mb-0 mt-3">Make Reports</h5>
+                <span className="count text-sm">100</span>
               </div>
             </div>
-          </a>  
+          </a>
         </div>
-        <div className="col-lg-2 col-md-2 col-12 mt-4 mt-md-0">
+
+        <div className="col-lg-2 col-md-4 col-6 mb-3">
           <a href="/quotation">
-            {" "}
             <div className="card">
               <span className="mask opacity-10 border-radius-lg"></span>
-              <div className="card-body p-3 position-relative">
-                <div className="row">
-                  <div className="col-12 text-center">
-                    <div className="icon_wrapper">
-                      <img
-                        src="https://cdn3d.iconscout.com/3d/premium/thumb/invoice-3d-illustration-download-in-png-blend-fbx-gltf-file-formats--bill-transaction-payment-purchase-business-pack-illustrations-3928170.png"
-                        alt=""
-                        className="client_img"
-                      />
-                    </div>
-                    <h5 className="font-weight-bolder mb-0 mt-3">
-                      Add Quotation
-                    </h5>
-                    <span className="count text-sm">100</span>
-                  </div>
+              <div className="card-body p-3 position-relative text-center">
+                <div className="icon_wrapper">
+                  <img
+                    src="https://cdn3d.iconscout.com/3d/premium/thumb/invoice-3d-illustration-download-in-png-blend-fbx-gltf-file-formats--bill-transaction-payment-purchase-business-pack-illustrations-3928170.png"
+                    alt=""
+                    className="client_img"
+                  />
                 </div>
+                <h5 className="font-weight-bolder mb-0 mt-3">Quotation</h5>
+                <span className="count text-sm">100</span>
               </div>
             </div>
           </a>
@@ -167,7 +124,7 @@ export default function Home() {
       </div>
 
       <div className="row my-4 ">
-        <div className="col-lg-8 col-md-6 mb-md-0 mb-4">
+        <div className="col-lg-12 col-md-12 mb-md-0 mb-4">
           <div className="card">
             <div className="card-header pb-0">
               <div className="row">
@@ -288,7 +245,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="col-lg-4 col-md-6">
+        {/* <div className="col-lg-4 col-md-6">
           <div className="card h-100">
             <div className="card-header pb-0">
               <h6>Daily Activity</h6>
@@ -303,15 +260,15 @@ export default function Home() {
                     <i className="fa fa-file-invoice text-success"></i>
                   </span>
                   <div className="timeline-content">
-                  <h6 className="text-primary text-sm mb-1">Voucher</h6>
+                    <h6 className="text-primary text-sm mb-1">Voucher</h6>
 
                     <h6 className="text-dark text-sm font-weight-bold mb-0">
                       $2400, Design changes
                     </h6>
                     <p className="text-secondary font-weight-bold text-xs mt-1 mb-0">
                       Added by: <span className="text-dark">John Doe</span> |
-                       <span className="text-dark">22 DEC 2024</span> |
-                       <span className="text-dark">7:20 PM</span>
+                      <span className="text-dark">22 DEC 2024</span> |
+                      <span className="text-dark">7:20 PM</span>
                     </p>
                   </div>
                 </div>
@@ -323,41 +280,40 @@ export default function Home() {
                     <i className="fa fa-box-open text-info"></i>
                   </span>
                   <div className="timeline-content">
-                  <h6 className="text-primary text-sm mb-1">Stock Item</h6>
+                    <h6 className="text-primary text-sm mb-1">Stock Item</h6>
                     <h6 className="text-dark text-sm font-weight-bold mb-0">
                       Stock replenished
                     </h6>
                     <p className="text-secondary font-weight-bold text-xs mt-1 mb-0">
                       Added by: <span className="text-dark">Jane Smith</span> |
-                       <span className="text-dark">21 DEC 2024</span> |
-                      Time: <span className="text-dark">9:34 PM</span>
+                      <span className="text-dark">21 DEC 2024</span> | Time:{" "}
+                      <span className="text-dark">9:34 PM</span>
                     </p>
                   </div>
                 </div>
               </div>
 
-             
               <div className="timeline timeline-one-side mb-4">
                 <div className="timeline-block">
                   <span className="timeline-step">
                     <i className="fa fa-user-plus text-primary"></i>
                   </span>
                   <div className="timeline-content">
-                  <h6 className="text-primary text-sm mb-1">Client</h6>
+                    <h6 className="text-primary text-sm mb-1">Client</h6>
                     <h6 className="text-dark text-sm font-weight-bold mb-0">
                       New client added
                     </h6>
                     <p className="text-secondary font-weight-bold text-xs mt-1 mb-0">
                       Added by: <span className="text-dark">Michael Brown</span>{" "}
-                      |  <span className="text-dark">18 DEC 2024</span> |
-                      Time: <span className="text-dark">4:54 AM</span>
+                      | <span className="text-dark">18 DEC 2024</span> | Time:{" "}
+                      <span className="text-dark">4:54 AM</span>
                     </p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </>
   );
