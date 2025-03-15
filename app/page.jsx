725c2f11c2e5da5c24ad579f5071093231235711
Satish jassal <thesatishjassal@gmail.com>
2025-03-15 13:50:43 +0530

    code

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,13 @@
+import { useEffect } from "react";
+
 const Welcome = () => {
-  return <h3>Welcome !</h3>;
+  useEffect(() => {
+    if (window.location.hostname === "www.panvik.in") {
+      window.location.href = "https://www.panvik.com/";
+    }
+  }, []);
+
+  return <h3>Welcome!</h3>;
 };
 
 export default Welcome;
