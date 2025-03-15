4cc481fe99a894e16f2977b196da45119b34ee39
Satish jassal <thesatishjassal@gmail.com>
2025-03-15 14:02:30 +0530

    code

--- app/page.jsx
+++ app/page.jsx
@@ -6,9 +6,7 @@ import { useEffect } from "react";
 const Welcome = () => {
   useEffect(() => {
     if (typeof window !== "undefined" && window.location.hostname === "www.panvik.in") {
-      setTimeout(() => {
         window.location.href = "https://www.panvik.com/";
-      }, 1000); // 2 seconds delay
     }
   }, []); // Empty dependency array to run only once on mount
 
