c953164c6c8c9d1969588dce816c25a6fa1363ef
Satish jassal <thesatishjassal@gmail.com>
2025-03-15 14:01:04 +0530

    code

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,16 @@
 "use client"
+"use client"; // 👈 This makes sure this component runs only on client side
+
 import { useEffect } from "react";
 
 const Welcome = () => {
   useEffect(() => {
-    if (window.location.hostname === "www.panvik.in") {
-      window.location.href = "https://www.panvik.com/";
+    if (typeof window !== "undefined" && window.location.hostname === "www.panvik.in") {
+      setTimeout(() => {
+        window.location.href = "https://www.panvik.com/";
+      }, 1000); // 2 seconds delay
     }
-  }, [window.location.hostname]);
+  }, []); // Empty dependency array to run only once on mount
 
   return <h3>Welcome!</h3>;
 };
