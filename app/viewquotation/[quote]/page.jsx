9643d40ff8e65a612e68d9eb8c2be6bb0f559c9a
Satish jassal <thesatishjassal@gmail.com>
2025-08-29 10:48:17 +0530

    first commit

--- app/viewquotation/[quote]/page.jsx
+++ app/viewquotation/[quote]/page.jsx
@@ -87,7 +87,7 @@ export default async function ViewQuotation({ params }) {
                     <b className="tm_primary_color">Client Details:</b>
                   </p>
                   <p>
-                    Business Name: <b>{client.businessname}</b> <br />
+                    {/* Business Name: <b>{client.businessname}</b> <br /> */}
                     Name: <b>{client.client_name}</b> <br />
                     City: <b>{client.city}</b> | Phone: <b>{client.client_phone}</b>
                   </p>
