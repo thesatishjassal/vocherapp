95b2ef7499a3774293579fea0df680b118fb0972
Satish jassal <thesatishjassal@gmail.com>
2026-02-13 13:19:40 +0530

    sales order

--- app/addquotation/page.jsx
+++ app/addquotation/page.jsx
@@ -170,7 +170,7 @@ useEffect(() => {
 
       const quotationData = {
         quotation_no: generateQuotationNumber,
-        salesperson: quotationInfo?.Salesperson || "Unknown Salesperson",
+        salesperson: quotationInfo?.Salesperson ,
         subject: quotationInfo?.Subject || "Quotation for Products/Services",
         amount_including_gst: Math.round(gstDetails.totalWithGST) || 0,
         without_gst: Math.round(gstDetails.withoutGST) || 0,
